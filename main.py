76aa02071f907f42686d37672abe425a93158dd7
WFrazierIII <wwfrazier@gmail.com>
2021-02-05 15:21:02 -0700

    Updated to contain a function and a return statement.

--- main.py
+++ main.py
@@ -1,34 +1,25 @@
-# Project Goal: Import a readable sales agreement and then scan it for keywords.
-# Highlight keywords in PDF and create a text document with a list of the keywords and the words surrounding them.
+# todo Project Goal: Import a readable sales agreement and then scan it for keywords.
+# todo Highlight keywords in PDF and create a text document with a list of the keywords and the words surrounding them.
+# todo recognize tax schedule, identify fields, copy field to income analysis form.
 
 import PyPDF2
 import tkinter
-import os
-import sys
-
-original_stdout = sys.stdout # Save a reference to the original standard output
-sys.stdout = open('TestPDF.txt', 'w+')
 
 from tkinter import filedialog
 
 root = tkinter.Tk()
 root.withdraw()
 
-filename = filedialog.askopenfilename(parent=root,title='Choose File Path') #Credit: https://stackoverflow.com/questions/3643418/storing-file-path-using-windows-explorer-browser-in-python
+def key_search():
+    filename = filedialog.askopenfilename(parent=root, title='Choose File Path')  # Credit: https://stackoverflow.com/questions/3643418/storing-file-path-using-windows-explorer-browser-in-python
+    with open(filename, mode='rb') as f: #rb = read only binary, 'f' is a custom file object. Credit: https://www.soudegesu.com/en/post/python/extract-text-from-pdf-with-pypdf2/
+        reader = PyPDF2.PdfFileReader(f)
+        for page in reader.pages:
+            return(page.extractText())
 
-with open(filename, mode='rb') as f: #rb = read only binary, 'f' is a custom file object. Credit: https://www.soudegesu.com/en/post/python/extract-text-from-pdf-with-pypdf2/
-    reader = PyPDF2.PdfFileReader(f)
-    #(1)sys.stdout = f  # Change the standard output to the file we created
-    for page in reader.pages:
-        pass
-    print(page.extractText())
-    #(1)sys.stdout = original_stdout  # Reset the standard output to its original value
-
+print(key_search())
 
 
-#t = open("TestPDF.txt", "w+") #(Replaced with os.system command for now)
-
-#os.system('notepad.exe ' + 'TestPDF.txt')
 
 
 
