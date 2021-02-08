e11c7ff4f31feb0095de32ce05ac491f5e45078d
wwfrazier <wwfrazier@gmail.com>
2021-02-08 10:41:34 -0700

    Created a list called results that can then be called from elsewhere in the code

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from tkinter import filedialog
 root = tkinter.Tk()
 root.withdraw()
 
+test_sheet = 'C:\Users\wwfra\Desktop\Frazier_Python_Test.csv'
+
 def key_search():
     filename = filedialog.askopenfilename(parent=root, title='Choose File Path')  # Credit: https://stackoverflow.com/questions/3643418/storing-file-path-using-windows-explorer-browser-in-python
     with open(filename, mode='rb') as f: #rb = read only binary, 'f' is a custom file object. Credit: https://www.soudegesu.com/en/post/python/extract-text-from-pdf-with-pypdf2/
@@ -19,13 +21,13 @@ def key_search():
             results.append(page.extractText())
         return results
 
+def xl_write():
+    with open(test_sheet, 'w') as g:
+        csvwt = csv.writer(g)
+        for x in key_search():
+            csvwt.writerow(x)
+
 pages = key_search()
 for page in pages:
     print(page)
-
-
-
-
-
-
 
